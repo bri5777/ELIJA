b0a1fde2ed4a09f9b3c0de85181346d321431543
bri5777 <elijahombogo79@gmail.com>
2024-10-31 17:20:21 +0300

    Update nr.js

--- plugins/nr.js
+++ plugins/nr.js
@@ -1,17 +1,17 @@
-const { bot, zushi, yami, ope, jidToNum } = require('../lib/')
+const { bot, elija, glen, ninja, jidToNum } = require('../lib/')
 
 bot(
   {
-    pattern: 'zushi ?(.*)',
+    pattern: 'elija ?(.*)',
     desc: 'allow set commands to be used by others in chat',
     type: 'logia',
   },
   async (message, match) => {
     if (!match)
       return await message.send(
-        `> Example :\n- zushi ping, sticker\n\nwanna set all ? type list copy and paste the reply message\n- zushi copied_message`
+        `> Example :\n- elija ping, sticker\n\nwanna set all ? type list copy and paste the reply message\n- elija copied_message`
       )
-    const z = await zushi(match, message.jid, message.id)
+    const z = await elija(match, message.jid, message.id)
     if (!z) return await message.send(`*${match}* already set`)
 
     await message.send(
@@ -25,12 +25,12 @@ bot(
 
 bot(
   {
-    pattern: 'yami ?(.*)',
+    pattern: 'glen ?(.*)',
     desc: 'shows the commands',
     type: 'logia',
   },
   async (message, match) => {
-    const z = await yami(message.jid, message.id)
+    const z = await glen(message.jid, message.id)
     if (!z || !z.length) return await message.send(`not set any`)
     await message.send(
       `*allowed commands for @${message.isGroup ? message.jid : jidToNum(message.jid)}*\n${z
@@ -43,13 +43,13 @@ bot(
 
 bot(
   {
-    pattern: 'ope ?(.*)',
+    pattern: 'ninja ?(.*)',
     desc: 'delete or unset the command',
     type: 'logia',
   },
   async (message, match) => {
-    if (!match) return await message.send('> Example :\n- ope ping, sticker\n- ope all')
-    const z = await ope(message.jid, match, message.id)
+    if (!match) return await message.send('> Example :\n- ninja ping, sticker\n- ninja all')
+    const z = await ninja(message.jid, match, message.id)
     if (z === null) return await message.send(`not set *${match}*`)
     if (z === 'all') return await message.send(`_removed all allowed commands_`)
     await message.send(
